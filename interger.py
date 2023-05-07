05fca9c361f958a640ae74879a93d666fb72b115
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-05-07 10:53:40 +0300

    Integers in Python

---
+++ interger.py
@@ -0,0 +1,12 @@
+# Assign interger values to variables x and y.
+x = 1
+y = 2
+
+# Assign the sum of x and y to the variable z.
+z = x + y
+
+# Print the value of z.
+print(z)
+
+# Print the data type of z.
+print(type(z))
