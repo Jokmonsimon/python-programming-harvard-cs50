9172571eb90df8248419eed84ff09d7403fef305
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-05-07 11:15:17 +0300

    Working with integer in Python

--- interger.py
+++ interger.py
@@ -7,6 +7,28 @@ z = x + y
 
 # Print the value of z.
 print(z)
+print("-------------------------------------------------------")
 
 # Print the data type of z.
 print(type(z))
+
+# Integers and User Input in Python
+x1 = input("Enter the value of x1: ")
+y1 = input("Enter the value of y1: ")
+
+# Sum of x1 and y1
+z1 = x1 + y1
+print(type(z1))
+# Print the value of z1 and its data typeprint(type(z1))
+print(z1)
+print(type(z1))
+print("-------------------------------------------------------")
+
+# Convert user input to integer
+z1 = int(x1) + int(y1)
+print(z1)
+print(type(z1))
+print("-------------------------------------------------------")
+
+# Perform all the steps above in one line of code
+print(int(input("Enter the value of x1: ")) + int(input("Enter the value of y1: ")))
