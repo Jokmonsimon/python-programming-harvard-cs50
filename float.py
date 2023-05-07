69d43a99a8736e4f558d0c70c6ea36b21aa195e8
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-05-07 11:43:46 +0300

    Working with float in Python

---
+++ float.py
@@ -0,0 +1,26 @@
+# Float is a number with fractional digits
+# Sum two floats x and y
+x = 2.4
+y = 3.2
+z = x + y
+
+print(z)
+print(type(z))
+print("-------------------------------------------------------")
+
+# Ask user to input float values
+x = float(input("Enter the2.4 value x: "))
+y = float(input("Enter the value y: "))
+z = x + y
+
+print(z)
+print(type(z))
+print("-------------------------------------------------------")
+
+# Round the value of z
+z = round(x + y)
+
+print(z)
+print(f"{z:,}")
+print(type(z))
+print("-------------------------------------------------------")
