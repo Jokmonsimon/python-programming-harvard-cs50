39b1b6c4929fc53370db325c0f151a5cb49ad1eb
Ojok Simon Peter <simonpeter.oj@gmail.com>
2023-05-07 00:31:00 -0400

    Working with string

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+# Ask user for their name
+name = input("What is your name? ")
+
+# Remove whitespaces from string
+name = name.strip()
+
+# Capitalize user's name
+name = name.capitalize()
+name = name.upper()
+name = name.lower()
+
+# Say Hello user!
+print("Hello,", name)
+print("Hello, " + name)
+print("Hello, {}!".format(name))
